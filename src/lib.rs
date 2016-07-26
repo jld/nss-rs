88d3e955fa04005711da6367db94267e134d4696
Jed Davis <jld@mozilla.com>
2016-07-25 17:42:41 -0700

    Reorganize a little.

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,21 @@
 extern crate nss_sys;
+pub mod nspr;
 
-use nss_sys::{NSS_NoDB_Init, SECSuccess};
-use nss_sys::nspr::{PR_Init, PR_SYSTEM_THREAD, PR_PRIORITY_NORMAL};
+use nss_sys as sys;
 use std::ptr;
-use std::sync::{Once, ONCE_INIT};
 
 macro_rules! assert_success {
-    ($e:expr) => { assert_eq!(unsafe { $e }, SECSuccess) }
+    ($e:expr) => { assert_eq!(unsafe { $e }, sys::SECSuccess) }
 }
 
 // TODO: bind GetError and make these things return `Result`s.
 // TODO: What do I do about this init/shutdown stuff vs. lifetimes/safety?
 
 pub fn init() {
-    init_nspr();
-    assert_success!(NSS_NoDB_Init(ptr::null()));
+    nspr::init();
+    assert_success!(sys::NSS_NoDB_Init(ptr::null()));
 }
 
-fn init_nspr() {
-    // NSPR initialization is done implicitly on the first call to any
-    // API, but this "run once" check is thread-unsafe -- NSPR's
-    // equivalent of `Once` can't be used because its threading layer
-    // isn't initialized yet, and its atomics are polyfilled with
-    // thread mutexes on some platforms so they're also off-limits.
-    static PR_INIT_ONCE: Once = ONCE_INIT;
-    PR_INIT_ONCE.call_once(|| {
-        // These argument values haven't been used since before NSPR
-        // was released as open source, but fill in something reasonable.
-        unsafe { PR_Init(PR_SYSTEM_THREAD, PR_PRIORITY_NORMAL, 0) };
-    });
-}
 
 #[cfg(test)]
 mod tests {
---
+++ src/nspr.rs
@@ -0,0 +1,18 @@
+use nss_sys::nspr as sys;
+use std::sync::{Once, ONCE_INIT};
+
+pub fn init() {
+    // NSPR initialization is done implicitly on the first call to any
+    // API, but this "run once" check is thread-unsafe -- NSPR's
+    // equivalent of `Once` can't be used because its threading layer
+    // isn't initialized yet, and its atomics are polyfilled with
+    // thread mutexes on some platforms so they're also off-limits.
+    static PR_INIT_ONCE: Once = ONCE_INIT;
+    PR_INIT_ONCE.call_once(|| {
+        // These argument values haven't been used since before NSPR
+        // was released as open source, but fill in something reasonable.
+        unsafe {
+            sys::PR_Init(sys::PR_SYSTEM_THREAD, sys::PR_PRIORITY_NORMAL, 0);
+        }
+    });
+}
